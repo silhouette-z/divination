141c656ae7e2da4cadb3e359a300aded4c6b200e
unknown <1223889859@qq.com>
2021-12-09 17:33:21 +0800

    头像

--- app/src/main/java/com/me/myapplication/activity/SettingActivity.kt
+++ app/src/main/java/com/me/myapplication/activity/SettingActivity.kt
@@ -1,25 +1,33 @@
 package com.me.myapplication.activity
 
-
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.loader.content.CursorLoader
+import com.bumptech.glide.Glide
+import com.google.android.material.internal.ContextUtils.getActivity
 import com.me.myapplication.R
 import kotlinx.android.synthetic.main.activity_setting.*
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.InputStream
+import java.io.*
+
+
+
+
 
 
 class SettingActivity : AppCompatActivity() {
     companion object{
         const val TAKE_PHOTO = 0
         const val CHOOSE_ALBUM = 1
+        const val HEADER_FILENAME = "/header.png"
     }
     var dialog: AlertDialog? = null
+    var context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +39,7 @@ class SettingActivity : AppCompatActivity() {
             dialog?.show()
             true
         }
+
     }
 
 
@@ -44,25 +53,37 @@ class SettingActivity : AppCompatActivity() {
 
                         }
                         CHOOSE_ALBUM -> {
-                            val openAlbumIntent = Intent(
-                                Intent.ACTION_GET_CONTENT)
-                            openAlbumIntent.setType("image/*")
-                            startActivityForResult(openAlbumIntent, CHOOSE_ALBUM)
-
+                            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                            intent.addCategory(Intent.CATEGORY_OPENABLE)
+                            intent.type = "image/*"
+                            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+                            startActivityForResult(intent, CHOOSE_ALBUM)
                         }
                     }
                 }
-
             }).create()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when(requestCode) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+         when (requestCode) {
             CHOOSE_ALBUM -> {
-
+                Log.d("uri",intent!!.data.toString())
+                Glide.with(context).load(intent.data).into(headerImage)
             }
         }
     }
+
+
+
+
+    fun isExternalStorageWritable(): Boolean {
+        return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+    }
+
+
+
+
+
 }
 
